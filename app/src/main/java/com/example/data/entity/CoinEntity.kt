9e37ac93912832735c52cd0e0821a95f7b67363d
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-01-15 12:59:15 +0300

    Added Coin Entity and updated build gradle with room dependencies

---
+++ app/src/main/java/com/example/data/entity/CoinEntity.kt
@@ -0,0 +1,39 @@
+package com.example.data.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = CoinEntity.TABLE)
+data class CoinEntity(
+    @ColumnInfo(name = IS_ACTIVE)
+    var isActive: Boolean,
+    @ColumnInfo(name = IS_NEW)
+    var isNew: Boolean,
+    @ColumnInfo(name = NAME)
+    var name: String,
+    @ColumnInfo(name = RANK)
+    var rank: Int,
+    @ColumnInfo(name = SYMBOL)
+    var symbol: String,
+    @ColumnInfo(name = TYPE)
+    var type: String
+) {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = ID)
+    var id: Long = 0
+
+    companion object {
+        const val TABLE = "coin_entity_table"
+
+        const val ID = "_id"
+        const val IS_ACTIVE = "is_active"
+        const val IS_NEW = "is_new"
+        const val NAME = "name"
+        const val RANK = "rank"
+        const val SYMBOL = "symbol"
+        const val TYPE = "type"
+
+    }
+}
