ed23e2a81a15b5b007d02102a05280634bc57a00
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2023-01-15 13:48:43 +0300

    updated dao with more queries

--- app/src/main/java/com/example/daotestingmedium/data/dao/TestingDao.kt
+++ app/src/main/java/com/example/daotestingmedium/data/dao/TestingDao.kt
@@ -5,15 +5,28 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.daotestingmedium.data.entity.CoinEntity
+import com.example.daotestingmedium.data.entity.ExchangesEntity
+import com.example.daotestingmedium.data.entity.TickersEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TestingDao {
 
     @Query("SELECT * FROM coin_entity_table")
-    fun getAllPlanterInfo(): Flow<List<CoinEntity>>
+    fun getCoinList(): Flow<List<CoinEntity>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertPlanterInfo(coinEntity: CoinEntity): Long
+    suspend fun insertCoin(coinEntity: CoinEntity): Long
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertExchanges(exchangesEntity: ExchangesEntity): Long
+
+    @Query("SELECT * FROM exchanges_entity_table WHERE _id = :id")
+    suspend fun getExchangesById(id: Long): ExchangesEntity
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTickers(tickersEntity: TickersEntity): Long
+
+    @Query("SELECT * FROM tickers_entity_table WHERE _id = :id")
+    suspend fun getTickersById(id: Long): TickersEntity
 }
